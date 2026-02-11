6c47a6e2c32d7be93d4e7dc14eba942645feef12
rajithapeddagangannagari7337 <rajithapeddagangannagari7337@gmail.com>
2026-02-11 13:29:20 +0530

    commit

--- src/main/java/org/MovieBookingApp/controller/BookingController.java
+++ src/main/java/org/MovieBookingApp/controller/BookingController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/bookings") // Base URL: localhost:8080/bookings
+@RequestMapping("/bookings")
 public class BookingController {
 
     @Autowired
--- src/main/java/org/MovieBookingApp/service/PaymentGatewayService.java
+++ src/main/java/org/MovieBookingApp/service/PaymentGatewayService.java
@@ -1,3 +1,4 @@
+
 package org.MovieBookingApp.service;
 
 import com.stripe.Stripe;
